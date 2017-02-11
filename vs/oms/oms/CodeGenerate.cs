671a68652241167bbbd99c91922de0b8bcb873ed
onemore <treertzhu@123u.com>
2017-02-11 12:55:45 +0800

    代码生成文件

---
+++ vs/oms/oms/CodeGenerate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oms
+{
+    /// <summary>
+    /// 生成函数Funtion
+    /// </summary>
+    class CodeGenerate
+    {
+        void Reset()
+        {
+
+        }
+        void EnterFunction()
+        {
+
+        }
+        void LeaveFunction()
+        {
+
+        }
+        void GetFunction()
+        {
+
+        }
+
+        void ParseChunk(Chunk tree)
+        {
+            EnterFunction();
+            {
+                
+            }
+            LeaveFunction();
+        }
+    }
+}
